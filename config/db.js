37c6786b1def70b57a0b817172b1fc55274290c1
Jai Shree <jaishree2023.07@gmail.com>
2026-04-02 09:39:37 +0530

    deploy ready

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: 40205,
     dialect: "mysql",
     logging: false, // Optional: remove noisy logs
   }
